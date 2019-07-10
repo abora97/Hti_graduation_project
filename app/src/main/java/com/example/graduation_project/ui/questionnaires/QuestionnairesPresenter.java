f32a92f44381746fa506bfc944c79400c1f6b227
nehal <nehalgamal63@gmail.com>
2019-07-10 04:01:46 +0200

    chinge layout note in activty questionnaires

--- app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesPresenter.java
+++ app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesPresenter.java
@@ -1,4 +1,19 @@
 package com.example.graduation_project.ui.questionnaires;
 
-public class QuestionnairesPresenter {
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class QuestionnairesPresenter extends  QuestionnairesActivity {
+
+
+    public void QuestionnairesPresenter (int Radio_group_id){
+int radioGriub=Radio_group_id;
+        RadioGroup radioGroup = (RadioGroup) findViewById(Radio_group_id);
+        int Selsct_id=radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton_selected=findViewById(Selsct_id);
+        Toast.makeText(this,"select radio botton"+radioButton_selected.getText(),Toast.LENGTH_SHORT).show();
+
+
+    }
 }
