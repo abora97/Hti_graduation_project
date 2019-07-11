57131c149b91b916e2f5469a07a165ad5264d719
nehal <nehalgamal63@gmail.com>
2019-07-11 20:26:14 +0200

    radio test

--- app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesActivity.java
+++ app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesActivity.java
@@ -3,6 +3,7 @@ package com.example.graduation_project.ui.questionnaires;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.autofill.AutofillValue;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,34 +16,43 @@ import static android.widget.Toast.LENGTH_SHORT;
 
 public class QuestionnairesActivity extends AppCompatActivity {
 
-    TextView textView7,textView8,textView9,textView10,textView11 ;
-    Button submit ;
+    TextView textView7, textView8, textView9, textView10, textView11;
+    Button submit;
     RadioGroup RadioGroup_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaires);
-        textView7=findViewById(R.id.textView7);
-        textView8=findViewById(R.id.textView8);
-        textView9=findViewById(R.id.textView9);
-        textView10=findViewById(R.id.textView10);
-        textView11=findViewById(R.id.textView11);
-        RadioGroup_1=findViewById(R.id.RadioGroup_1);
-        submit=findViewById(R.id.submit);
+        textView7 = findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+        textView9 = findViewById(R.id.textView9);
+        textView10 = findViewById(R.id.textView10);
+        textView11 = findViewById(R.id.textView11);
+        RadioGroup_1 = findViewById(R.id.RadioGroup_1);
+        submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int Selsct_id=RadioGroup_1.getCheckedRadioButtonId();
-                RadioButton radioButton_selected=findViewById(Selsct_id);
-             if (RadioGroup_1.isClickable()) {
-                 Toast.makeText(QuestionnairesActivity.this,"Thanks ",Toast.LENGTH_SHORT).show();
+
+                //   QuestionnairesPresenter questionnairesPresenter = new QuestionnairesPresenter();
+
+                //    questionnairesPresenter.QuestionnairesPresenter(R.id.RadioGroup_1);
+
+                int Selsct_id = RadioGroup_1.getCheckedRadioButtonId();
+                RadioButton radioButton_selected = findViewById(Selsct_id);
+                if (radioButton_selected == null) {
+                    Toast.makeText(QuestionnairesActivity.this, "please select all Question ", Toast.LENGTH_SHORT).show();
 
 
-             }else{
-                Toast.makeText(QuestionnairesActivity.this,"please select all Question ",Toast.LENGTH_SHORT).show();
-            }}
+                } else {
+                    Toast.makeText(QuestionnairesActivity.this, " Thanks", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+
     }
+
 
 }
--- app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesPresenter.java
+++ app/src/main/java/com/example/graduation_project/ui/questionnaires/QuestionnairesPresenter.java
@@ -8,11 +8,14 @@ public class QuestionnairesPresenter extends  QuestionnairesActivity {
 
 
     public void QuestionnairesPresenter (int Radio_group_id){
-int radioGriub=Radio_group_id;
-        RadioGroup radioGroup = (RadioGroup) findViewById(Radio_group_id);
+        RadioGroup radioGroup = findViewById(Radio_group_id);
         int Selsct_id=radioGroup.getCheckedRadioButtonId();
         RadioButton radioButton_selected=findViewById(Selsct_id);
-        Toast.makeText(this,"select radio botton"+radioButton_selected.getText(),Toast.LENGTH_SHORT).show();
+        if (radioButton_selected==null) {
+            Toast.makeText(this,"please select all Question ",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this," Thanks",Toast.LENGTH_SHORT).show();
+        }
 
 
     }
