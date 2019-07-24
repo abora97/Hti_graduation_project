c0b5b5f9a372ffa41e8d191e06cc093b8ee2f22e
mahmoud <mahmoud.elebiary@yahoo.com>
2019-07-24 03:44:39 +0200

    update api url

--- app/src/main/java/com/example/graduation_project/util/Constant.java
+++ app/src/main/java/com/example/graduation_project/util/Constant.java
@@ -3,13 +3,13 @@ package com.example.graduation_project.util;
 public interface Constant {
 
 
-    String LOGIN_URL = "https://graduationhti.000webhostapp.com/public/api/login";
+    String LOGIN_URL = "http://questcomp.ml/public/api/login";
     String ID = "c_id";
     String PASSWORD = "password";
-    String SUBJECT_URL="https://graduationhti.000webhostapp.com/public/api/subjects";
+    String SUBJECT_URL="http://questcomp.ml/public/api/subjects";
     String AUTHORIZATION = "Authorization";
 
-    String COMPLAIN_URL="https://graduationhti.000webhostapp.com/public/api/complain";
+    String COMPLAIN_URL="http://questcomp.ml/public/api/complains";
     String COMPLAIN_DEPART="department";
     String COMPLAIN_TYPE="complain_type";
     String COMPLAIN_TOPIC="topic";
